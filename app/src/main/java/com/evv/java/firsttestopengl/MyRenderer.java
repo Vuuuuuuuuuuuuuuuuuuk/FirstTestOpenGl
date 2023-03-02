57c92d585cf59957e0caabb76bd091a7a80939ec
Vuuuuuuuuuuuuuuuuuuk <baberlaybabs@gmail.com>
2023-03-02 13:12:32 +0200

    part5

--- app/src/main/java/com/evv/java/firsttestopengl/MyRenderer.java
+++ app/src/main/java/com/evv/java/firsttestopengl/MyRenderer.java
@@ -27,7 +27,49 @@ public class MyRenderer implements GLSurfaceView.Renderer {
   public MyRenderer(Context context) {
     this.context = context;
 
-    float vertexes[] = new float[]{};
+    float vertexes[] = new float[]{
+      //triangle
+      -0.5f, 0.5f,
+      -0.25f, 0.75f,
+      -0.75f, 0.75f,
+
+      //triangle_fan
+      0.5f, 0.5f,
+      0.25f, 0.25f,
+      0.75f, 0.25f,
+      0.75f, 0.75f,
+      0.25f, 0.75f,
+      0.25f, 0.25f,
+
+      //triangle_strip
+      0.2f, -0.6f,
+      0.2f, -0.2f,
+      0.4f, -0.6f,
+      0.6f, -0.2f,
+      0.8f, -0.6f,
+
+      //line
+      -0.9f, -0.1f,
+      -0.7f, -0.1f,
+      -0.6f, -0.1f,
+      -0.4f, -0.1f,
+
+      //Line_strip
+      -0.9f, -0.3f,
+      -0.7f, -0.2f,
+      -0.6f, -0.3f,
+      -0.4f, -0.2f,
+
+      //Line_loop
+      -0.9f, -0.6f,
+      -0.7f, -0.5f,
+      -0.6f, -0.6f,
+      -0.4f, -0.5f,
+
+      //points
+      0.4f, -0.4f,
+      0.5f, -0.4f,
+    };
 
     vertexesData = ByteBuffer.allocateDirect(vertexes.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
     vertexesData.put(vertexes);
@@ -59,6 +101,27 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 
   @Override
   public void onDrawFrame(GL10 gl) {
+    glClear(GL_COLOR_BUFFER_BIT);
 
+    glUniform4f(uColorLocation,0.0f, 0.3f, 0.0f, 0.0f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glUniform4f(uColorLocation, 0.5f, 0.6f, 0.0f, 0.0f);
+    glDrawArrays(GL_TRIANGLE_FAN, 3, 6);
+
+    glUniform4f(uColorLocation, 0.5f, 0.6f, 0.8f, 0.0f);
+    glDrawArrays(GL_TRIANGLE_STRIP, 9, 5);
+
+    glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 0.0f);
+    glDrawArrays(GL_LINES, 14, 4);
+
+    glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 0.0f);
+    glDrawArrays(GL_LINE_STRIP, 18, 4);
+
+    glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 0.0f);
+    glDrawArrays(GL_LINE_LOOP, 22, 4);
+
+    glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+    glDrawArrays(GL_POINTS, 26, 2);
   }
 }
